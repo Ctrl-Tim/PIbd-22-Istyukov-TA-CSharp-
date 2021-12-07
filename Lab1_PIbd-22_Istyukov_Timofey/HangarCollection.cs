eb5e7a11ecc7c7ae724ca885804e2c5b1addf223
Ctrl-Tim <timon.istiukov@mail.ru>
2021-12-07 23:20:41 +0400

    commit 0

--- Lab1_PIbd-22_Istyukov_Timofey/HangarCollection.cs
+++ Lab1_PIbd-22_Istyukov_Timofey/HangarCollection.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.IO;
 
 namespace Lab1_PIbd_22_Istyukov_Timofey
 {
@@ -31,11 +30,6 @@ namespace Lab1_PIbd_22_Istyukov_Timofey
         /// Высота окна отрисовки
         /// </summary>
         private readonly int pictureHeight;
-
-        /// <summary>
-        /// Разделитель для записи информации в файл
-        /// </summary>
-        private readonly char separator = ':';
 
         /// <summary>
         /// Конструктор
@@ -91,111 +85,6 @@ namespace Lab1_PIbd_22_Istyukov_Timofey
                 {
                     return null;
                 }
-            }
-        }
-
-        /// <summary>
-        /// Метод записи информации в файл
-        /// </summary>
-        /// <param name="text">Строка, которую следует записать</param>
-        /// <param name="stream">Поток для записи</param>
-        private void WriteToFile(string text, FileStream stream)
-        {
-            byte[] info = new UTF8Encoding(true).GetBytes(text);
-            stream.Write(info, 0, info.Length);
-        }
-
-        /// <summary>
-        /// Сохранение информации по самолётам в ангаре в файл
-        /// </summary>
-        /// <param name="filename">Путь и имя файла</param>
-        /// <returns></returns>
-        public bool SaveData(string filename)
-        {
-            if (File.Exists(filename))
-            {
-                File.Delete(filename);
-            }
-            using (StreamWriter fs = new StreamWriter(filename))
-            {
-                fs.Write($"HangarCollection{Environment.NewLine}");
-                foreach (var level in hangarStages)
-                {
-                    //Начинаем парковку
-                    fs.Write($"Hangar{separator}{level.Key}{Environment.NewLine}");
-                    fs.Write(Environment.NewLine);
-                    ITransport air = null;
-                    for (int i = 0; (air = level.Value.GetNext(i)) != null; i++)
-                    {
-                        if (air != null)
-                        {
-                            //если место не пустое
-                            //Записываем тип самолёта
-                            if (air.GetType().Name == "WarPlane")
-                            {
-                                fs.Write($"WarPlane{separator}");
-                            }
-                            if (air.GetType().Name == "FighterPlane")
-                            {
-                                fs.Write($"FighterPlane{separator}");
-                            }
-                            //Записываемые параметры
-                            fs.Write(air + Environment.NewLine);
-                        }
-                    }
-                }
-            }
-            return true;
-        }
-
-        /// <summary>
-        /// Загрузка нформации по автомобилям на парковках из файла
-        /// </summary>
-        /// <param name="filename"></param>
-        /// <returns></returns>
-        public bool LoadData(string filename)
-        {
-            if (!File.Exists(filename))
-            {
-                return false;
-            }
-            using (StreamReader fs = new StreamReader(filename))
-            {
-                UTF8Encoding temp = new UTF8Encoding(true);
-                string strs = fs.ReadLine();
-                if (!strs.Contains("HangarCollection"))
-                {
-                    //если нет такой записи, то это не те данные
-                    return false;
-                }
-                hangarStages.Clear(); //если есть, то очищаем записи
-                Vehicle air = null;
-                string key = string.Empty;
-                while ((strs = fs.ReadLine()) != null)
-                {
-                    //идём по считанным записям
-                    if (strs.Contains("Hangar"))
-                    {
-                        key = strs.Split(separator)[1];
-                        hangarStages.Add(key, new Hangar<Vehicle>(pictureWidth, pictureHeight));
-                    }
-                    else if (strs.Contains(separator))
-                    {
-                        if (strs.Contains("WarPlane"))
-                        {
-                            air = new WarPlane(strs.Split(separator)[1]);
-                        }
-                        if (strs.Contains("FighterPlane"))
-                        {
-                            air = new FighterPlane(strs.Split(separator)[1]);
-                        }
-                        if (hangarStages[key] + air != 1)
-                        {
-                            return false;
-                        }
-                    }
-                }
-                return true;
             }
         }
     }
