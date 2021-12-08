6792c3fda5f2accdcd87c0f69e18a22a7c2ae21e
Ctrl-Tim <timon.istiukov@mail.ru>
2021-12-08 13:45:14 +0400

    commit 1

---
+++ Lab1_PIbd-22_Istyukov_Timofey/HangarOverflowException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab1_PIbd_22_Istyukov_Timofey
+{
+    /// <summary>
+    /// Класс-ошибка "Если в ангаре уже заняты все места"
+    /// </summary> 
+    class HangarOverflowException : Exception
+    {
+        public HangarOverflowException() : base("В ангаре нет свободных мест") { }
+    }
+}
