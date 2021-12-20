3b0e6909319d49fd1b5c54be0dcb10f1fbd4036a
Ctrl-Tim <timon.istiukov@mail.ru>
2021-12-20 19:24:40 +0400

    commit 2

---
+++ Lab1_PIbd-22_Istyukov_Timofey/HangarAlreadyHaveException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab1_PIbd_22_Istyukov_Timofey
+{
+    /// <summary>
+    /// Класс-ошибка "Если в ангаре уже есть самолёт с такими же характеристиками"
+    /// </summary>   
+    class HangarAlreadyHaveException : Exception
+    {
+        public HangarAlreadyHaveException() : base("В ангаре уже есть такой самолёт") { }
+    }
+}
